30c4d8271bc3612a9851668c19a017ce73cad1d7
natalielogi <logilogi93@yandex.ru>
2025-04-03 20:17:44 +0300

    подключила webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development", // Для разработки; для финальной сборки можно установить 'production'
+  entry: "./index.js", // путь к твоему основному файлу, например, index.js в папке src
+  output: {
+    filename: "bundle.js", // имя сгенерированного файла
+    path: path.resolve(__dirname, "dist"), // папка, куда будет помещён bundle.js
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"), // папка, откуда сервер будет отдавать файлы
+    compress: true,
+    port: 9000, // порт, на котором будет работать сервер
+    open: true, // автоматически откроет браузер при запуске сервера
+  },
+};
